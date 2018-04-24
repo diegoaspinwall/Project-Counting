94c6686f43ee7deafec233c2510897fc7bf363a8
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-04-24 12:42:07 -0400

    Updated from Brython Server: 4/24/2018 12:42:07 PM

--- jarProblem2.py
+++ jarProblem2.py
@@ -7,24 +7,6 @@ from random import randint
 w=10
 n=20
 
-#Strat1/2- Always red/green
-'''
-total=0
-redtotal=0
-
-for i in range(0,10000):
-    red=randint(0,n)
-    total+=n
-    redtotal+=red
-
-redprob=redtotal/total
-print('Prob of red',redprob)
-print('Prob of green',1-redprob)
-print('D =', redprob*(-1)*w)
-
-'''
-
-
 total=0
 picktotal=0
 
