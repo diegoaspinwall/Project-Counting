58c42d8ee414077e76604566ddc21f635a40a6ef
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-04-24 12:44:27 -0400

    Updated from Brython Server: 4/24/2018 12:44:27 PM

--- jarProblem3.py
+++ jarProblem3.py
@@ -1,1 +1,23 @@
+#Diego Aspinwall and Aidan
+#4-24-18
+#jarProblem3.py
 
+from random import randint
+
+w=10
+n=20
+
+#Strat4
+
+total=0
+redtotal=0
+
+for i in range(0,10000):
+    red=randint(0,n)
+    total+=n
+    redtotal+=red
+
+redprob=redtotal/total
+print('Prob of red',redprob)
+print('Prob of green',1-redprob)
+print('D =', redprob*(-1)*w)
