b28eff309f2e55056273c434b5bbc4ea76235b65
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-04-25 07:52:00 -0400

    Updated from Brython Server: 4/25/2018 7:51:59 AM

--- jarProblem3.py
+++ jarProblem3.py
@@ -7,7 +7,7 @@ from random import randint
 w=10
 n=20
 
-#Strat4
+#Strat5
 
 total=0
 picktotal=0
@@ -21,13 +21,15 @@ for i in range(0,1):
     for k in range(0,n-red):
         jar.append('g')
     count=0
-    if jar[randint(0,n-1)]=='r':
-        count+=1
-    if jar[randint(0,n-1)]=='r':
-        count+=1
+    for b in range(1,3):
+        rando = randint(0,n-b)
+        if jar[rando]=='r':
+            count+=1
+            jar.remove(jar[rando])
     if count==0 or count==2:
         color=randint(0,n-2)+2
         picktotal+=color
         print('yes')
     else:
         print('no')
+
