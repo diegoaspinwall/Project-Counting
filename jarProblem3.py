f5092964cd941f62b71aeaad2034aaa0340a196b
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-04-26 09:45:55 -0400

    Updated from Brython Server: 4/26/2018 9:45:54 AM

--- jarProblem3.py
+++ jarProblem3.py
@@ -41,7 +41,10 @@ pickprob=picktotal/total
 print('Prob of picked color',pickprob)
 '''
 
-for i in range(0,100):
+
+#Strat4
+
+for i in range(0,10000):
     red=randint(0,n)
     total+=1
     jar=[]
