2b0617927cb96603eed01a41776aae9cd9b17c76
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-04-26 12:06:44 -0400

    Updated from Brython Server: 4/26/2018 12:06:44 PM

--- jarProblem3.py
+++ jarProblem3.py
@@ -11,7 +11,7 @@ n=20
 
 total=0
 picktotal=0
-'''
+
 for i in range(0,10000):
     red=randint(0,n)
     total+=n
@@ -84,7 +84,7 @@ for i in range(0,1000):
 
 print(picktotal/total)
 
-
+'''
 
 #Strat4
 '''
