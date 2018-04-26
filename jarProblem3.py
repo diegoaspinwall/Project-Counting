288c82573f4a143e7fb4a4c9c9d841418ac3f92c
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-04-26 11:29:36 -0400

    Updated from Brython Server: 4/26/2018 11:29:36 AM

--- jarProblem3.py
+++ jarProblem3.py
@@ -41,9 +41,41 @@ pickprob=picktotal/total
 print('Prob of picked color',pickprob)
 '''
 
+for i in range(0,1):
+    red=randint(0,n)
+    total+=1
+    jar=[]
+    for j in range(0,red):
+        jar.append('r')
+    for k in range(0,n-red):
+        jar.append('g')
+    pick=[]
+    for b in range(1,3):
+        rando = randint(0,n-b)
+        if jar[rando]=='r':
+            pick.append('r')
+        else:
+            pick.append('g')
+        jar.remove(jar[rando])
+    print(pick)
+    if pick[0]==pick[1]:
+        guess=pick[0]
+        #print('same')
+    else:
+        rando = randint(0,n-1)
+        if jar[rando]=='r':
+            guess='r'
+        else:
+            guess='g'
+    #print(guess)
+    if jar[randint(0,n-1)]==guess:
+        picktotal+=1
+print(picktotal/total)
+
+
 
 #Strat4
-
+'''
 for i in range(0,10000):
     red=randint(0,n)
     total+=1
@@ -73,5 +105,5 @@ for i in range(0,10000):
     if jar[randint(0,n-1)]==guess:
         picktotal+=1
 print(picktotal/total)
-    
+'''
 
