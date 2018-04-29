26a87f3611123aa1b4126d024fd14a13f1a59f30
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-04-29 17:08:33 -0400

    Updated from Brython Server: 4/29/2018 5:08:33 PM

--- jarProblem3.py
+++ jarProblem3.py
@@ -12,9 +12,40 @@ n=20
 total=0
 picktotal=0
 
-
+#IF THE CHOICES ARE THE SAME, DIFF RANDOGUESSS
 #Strat4
 
+for i in range(0,10000):
+    red=randint(0,n)
+    total+=n
+    jar=[]
+    for j in range(0,red):
+        jar.append('r')
+    for k in range(0,n-red):
+        jar.append('g')
+    count=0
+    picks=[]
+    #print(jar)
+    for b in range(1,3):
+        rando = randint(0,n-1)
+        if jar[rando]=='r':
+            count+=1
+        picks.append(rando)
+        #print(jar)
+    if count==0 or count==2:
+        color=randint(0,n-2)+2
+        picktotal+=color
+        #print('two of same color')
+    else:
+        color=randint(0,n-3)+2
+        picktotal+=color
+        #print('two different, one more')
+
+pickprob=picktotal/total
+print('Prob of picked color',pickprob)
+
+
+'''
 for i in range(0,10000):
     red=randint(0,n)
     total+=1
@@ -44,7 +75,7 @@ for i in range(0,10000):
     if jar[randint(0,n-1)]==guess:
         picktotal+=1
 print(picktotal/total)
-
+'''
 
 print('R/S =', (w-5)*(1-(picktotal/total))+(-5)*(picktotal/total))
 #change -5 sometime?
