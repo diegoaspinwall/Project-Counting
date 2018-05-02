8aa53c2dcace3e0cb74cd1c172b691f9023a2136
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-05-01 22:29:20 -0400

    Updated from Brython Server: 5/1/2018 10:29:20 PM

--- jarProblemFinal.py
+++ jarProblemFinal.py
@@ -80,4 +80,40 @@ print('Prob of yes color strategy 4 =',twopickprob)
 R= (-1)*(w*twopickprob+D)
 print('R =', R)
 
+#Strat5
+fourtotal=0
+threepicktotal=0
+
+for i in range(0,10000):
+    red=randint(0,n)
+    fourtotal+=n
+    jar=[]
+    for j in range(0,red):
+        jar.append('r')
+    for k in range(0,n-red):
+        jar.append('g')
+    count=0
+    #print(jar)
+    for b in range(1,3):
+        rando = randint(0,n-b)
+        if jar[rando]=='r':
+            count+=1
+        jar.remove(jar[rando])
+        #print(jar)
+    if count==0 or count==2:
+        color=randint(0,n-2)+2
+        threepicktotal+=color
+        #print('two of same color')
+    else:
+        color=randint(0,n-3)+2
+        threepicktotal+=color
+        #print('two different, one more')
+
+threepickprob=threepicktotal/fourtotal
+print('Prob of picked color',threepickprob)
+
+
+
+
+
 
