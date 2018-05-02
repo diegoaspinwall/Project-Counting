0d09b12b0ecd338ace39d1d1c0ae48ce97b6fa29
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-05-01 20:17:56 -0400

    Updated from Brython Server: 5/1/2018 8:17:56 PM

--- jarProblemFinal.py
+++ jarProblemFinal.py
@@ -1,1 +1,21 @@
+#Diego Aspinwall and Aidan
+#5-2-18
+#jarProblemFinal.py
+
+from random import randint
+
+w=10
+n=20
+
+#Strat1/2- Always red/green
+
+total=0
+redtotal=0
+
+for i in range(0,10000):
+    red=randint(0,n)
+    total+=n
+    redtotal+=red
+
+redprob=redtotal/total
 
