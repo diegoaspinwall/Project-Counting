466ef1361a1f7b95071deb872f7ab907d3c3bb62
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-05-16 12:17:21 -0400

    Updated from Brython Server: 5/16/2018 12:17:20 PM

--- jarProblem2.py
+++ jarProblem2.py
@@ -11,7 +11,7 @@ n=20
 
 total=0
 picktotal=0
-
+'''
 for i in range(0,100000):
     color=randint(0,n-1)+1
     total+=n
@@ -19,3 +19,16 @@ for i in range(0,100000):
 
 pickprob=picktotal/total
 print('Prob of picked color',pickprob)
+'''
+
+
+for i in range(0,1):
+    red=randint(0,n)
+    total+=1
+    jar=[]
+    for j in range(0,red):
+        jar.append('r')
+    for k in range(0,n-red):
+        jar.append('g')
+    print(jar)
+
