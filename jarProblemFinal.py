63548e6f2d66d30b9f82bcd79a2d71a0572efa21
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-05-20 11:47:51 -0400

    Updated from Brython Server: 5/20/2018 11:47:51 AM

--- jarProblemFinal.py
+++ jarProblemFinal.py
@@ -83,28 +83,37 @@ threepicktotal=0
 
 for i in range(0,repeated):
     red=randint(0,n)
-    fourtotal+=n
+    fourtotal+=1
     jar=[]
+    unchangedjar=[]
     for j in range(0,red):
         jar.append('r')
+        unchangedjar.append('r')
     for k in range(0,n-red):
         jar.append('g')
-    count=0
-    #print(jar)
+        unchangedjar.append('g')
+    pick=[]
     for b in range(1,3):
         rando = randint(0,n-b)
         if jar[rando]=='r':
-            count+=1
+            pick.append('r')
+        else:
+            pick.append('g')
         jar.remove(jar[rando])
-        #print(jar)
-    if count==0 or count==2:
-        color=randint(0,n-2)+2
-        threepicktotal+=color
-        #print('two of same color')
+    if pick[0]==pick[1]:
+        guess=pick[0]
+        if unchangedjar[randint(0,n-1)]==guess:
+            threepicktotal+=1
     else:
-        color=randint(0,n-3)+2
-        threepicktotal+=color
-        #print('two different, one more')
+        rando = randint(0,n-3)
+        if jar[rando]=='r':
+            jar.remove(jar[rando])
+            guess='r'
+        else:
+            jar.remove(jar[rando])
+            guess='g'
+        if unchangedjar[randint(0,n-1)]==guess:
+            threepicktotal+=1
 
 threepickprob=threepicktotal/fourtotal
 print('Prob of yes color strategy 5',threepickprob)
